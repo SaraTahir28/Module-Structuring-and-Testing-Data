f3540a875de42c56a12122aff22b4e238430a220
Sara Tahir <saratahir34@gmail.com>
2025-06-06 11:04:35 +0100

    Updated what line 3 does

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -4,3 +4,12 @@ count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
+
+//line 3 has two parts, the right side is (count+1). Javascript will evaluate this expression first.
+//it will look up the current value of the count variable which is assigned as 0 and add 1 to 
+//it. This will update the count to 1.
+
+
+// The left side of the expression takes the result from right hand side and assign the value to
+//the variable count.
+// = is an assignment operator. It updates the variable on the left with the value of expression on the right.
